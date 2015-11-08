254751483364d1d13689ef074dbfdeea9d0f3757
haibei wu <haibei.wu@rbc.com>
2015-11-07 20:58:24 -0500

    complete timeline, goal and balance pages

--- js/morris-data.js
+++ js/morris-data.js
@@ -1,117 +1,98 @@
 $(function() {
-
-    Morris.Area({
+    var chartData = [];
+    var endDate = new Date();
+    var startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 1);
+    while(startDate <= endDate) {
+        var period = startDate.getFullYear() + "-" + (startDate.getMonth() + 1) + "-" + startDate.getDate();
+        var expense = Math.random() * 100;
+        var income = Math.random() * 100;
+        chartData.push({
+            period: period,
+            Expense: expense,
+            Income: income
+        })
+        startDate.setDate(startDate.getDate() + 1);
+    }
+    var chart = Morris.Area({
         element: 'morris-area-chart',
-        data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
-        }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
-        }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
-        }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
-        }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
-        }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
-        }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
-        }],
+        data: chartData,
+        parseTime: true,
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['Expense', 'Income'],
+        labels: ['<a href="timeline.html">Expense</a>', '<a href="timeline.html">Income</a>'],
         pointSize: 2,
         hideHover: 'auto',
+        dataFormat: function (x) {
+            return new Date(x).toString();
+        },
+        yLabelFormat: function(y) {
+            if(y >= 1000) {
+                y = (y/1000).toFixed(1) + "k";
+            }
+            return y.toFixed(1);
+        },
+        xLabels: "day",
         resize: true
     });
 
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales 1",
-            value: 12
-        }, {
-            label: "In-Store Sales 2",
-            value: 30
-        }, {
-            label: "Mail-Order Sales 3",
-            value: 20
-        }],
-        resize: true
-    });
+    chart.options.labels.forEach(function(label, i){
+        var textLabel = label.indexOf("Expense") > -1 ? "Expense" : label.indexOf("Income") > -1 ? "Income" : "";
+        var legendItem = $('<span></span>').text(textLabel).css('color', chart.options.lineColors[i]);
+        $('#legend').append(legendItem);
+    })
 
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
+    // Morris.Donut({
+    //     element: 'morris-donut-chart',
+    //     data: [{
+    //         label: "Download Sales 1",
+    //         value: 12
+    //     }, {
+    //         label: "In-Store Sales 2",
+    //         value: 30
+    //     }, {
+    //         label: "Mail-Order Sales 3",
+    //         value: 20
+    //     }],
+    //     resize: true
+    // });
+
+    // Morris.Bar({
+    //     element: 'morris-bar-chart',
+    //     data: [{
+    //         y: '2006',
+    //         a: 100,
+    //         b: 90
+    //     }, {
+    //         y: '2007',
+    //         a: 75,
+    //         b: 65
+    //     }, {
+    //         y: '2008',
+    //         a: 50,
+    //         b: 40
+    //     }, {
+    //         y: '2009',
+    //         a: 75,
+    //         b: 65
+    //     }, {
+    //         y: '2010',
+    //         a: 50,
+    //         b: 40
+    //     }, {
+    //         y: '2011',
+    //         a: 75,
+    //         b: 65
+    //     }, {
+    //         y: '2012',
+    //         a: 100,
+    //         b: 90
+    //     }],
+    //     xkey: 'y',
+    //     ykeys: ['a', 'b'],
+    //     labels: ['Series A', 'Series B'],
+    //     hideHover: 'auto',
+    //     resize: true
+    // });
 
 });
